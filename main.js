b2638be5d86345a44d2f82ec3b345bd3d7716238
clinton <clintonphillips464@gmail.com>
2023-08-17 20:17:26 +0100

    cleared some errors

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const menu = document.getElementById("menu");
-const mobile = document.getElementById("mobile");
-const menuTwo = document.getElementById("menuTwo");
+// const menu = document.getElementById("menu");
+// const mobile = document.getElementById("mobile");
+// const menuTwo = document.getElementById("menuTwo");
 
-document.onclick = function (e) {
-  if (e.target.id !== "mobile" && e.target.id !== "menu") {
-    menu.classList.remove("active");
-    mobile.classList.remove("active");
-    menuTwo.classList.toggle("active");
-  }
-};
+// document.onclick = function (e) {
+//   if (e.target.id !== "mobile" && e.target.id !== "menu") {
+//     menu.classList.remove("active");
+//     mobile.classList.remove("active");
+//     menuTwo.classList.toggle("active");
+//   }
+// };
 
-menuTwo.onclick = function () {
-  console.log("boy");
-  menu.classList.toggle("active");
-  mobile.classList.toggle("active");
-  menuTwo.classList.toggle("active");
-};
+// menuTwo.onclick = function () {
+//   console.log("boy");
+//   menu.classList.toggle("active");
+//   mobile.classList.toggle("active");
+//   menuTwo.classList.toggle("active");
+// };
 
 document.addEventListener("DOMContentLoaded", function () {
   // Set the target date to October 21, 2023
@@ -46,3 +46,51 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(updateCountdown, 1000); // Update the countdown every second
 });
 
+const me = document.getElementById('me');
+const mobile = document.getElementById('mobile');
+const ul = document.getElementById('ul')
+
+const clickHandler = () => {
+  me.classList.toggle('open');
+  mobile.classList.toggle('open');
+};
+
+me.addEventListener('click', clickHandler);
+
+document.addEventListener('click', (event) => {
+  const isMeClicked = me.contains(event.target);
+  const isMobileClicked = mobile.contains(event.target);
+
+  if (!isMeClicked && !isMobileClicked) {
+    me.classList.remove('open');
+    mobile.classList.remove('open');
+  }
+});
+ul.addEventListener('click', () => {
+  me.classList.remove('open');
+  mobile.classList.remove('open');
+})
+
+
+// var swiper = new Swiper(".mySwiper", {
+//   effect: "flip",
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+var swiper = new Swiper(".mySwiper", {
+  effect: "flip",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
